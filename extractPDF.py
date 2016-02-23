1a627e69e1069b99c99770895bb717f0a68b5391
Ankit Gohil <gohilankit@gmail.com>
2016-02-22 21:01:21 -0500

    Initial commit

---
+++ extractPDF.py
@@ -0,0 +1,36 @@
+import sys
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+
+def extract(args):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = file(args[0], 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    out = open(args[0]+'.txt', 'w')
+    out.write(text)
+
+
+    fp.close()
+    device.close()
+    retstr.close()
+    out.close()
+
+if __name__ == '__main__':
+    extract(sys.argv[1:])
