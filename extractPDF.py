ac96f466f2d487bb7b9898ddbff15d39e2e84138
Ankit Gohil <gohilankit@gmail.com>
2016-02-22 23:26:15 -0500

    Add trigrams

--- extractPDF.py
+++ extractPDF.py
@@ -1,4 +1,5 @@
 import sys
+import nltk
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -6,6 +7,7 @@ from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
 
 def extract(args):
+    print('In extract')
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
@@ -21,10 +23,13 @@ def extract(args):
     for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
         interpreter.process_page(page)
 
+
     text = retstr.getvalue()
+    tokens = nltk.word_tokenize(text)
+    trigrams = ngrams(tokens,3)
 
     out = open(args[0]+'.txt', 'w')
-    out.write(text)
+    out.write(trigrams)
 
 
     fp.close()
@@ -33,4 +38,5 @@ def extract(args):
     out.close()
 
 if __name__ == '__main__':
+    #nltk.download('punkt')
     extract(sys.argv[1:])
